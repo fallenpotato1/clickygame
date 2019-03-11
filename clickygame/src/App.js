81f7c5c720fae526c4f4fb578646bc3912d6a249
fallen potato <cruzjon32@gmail.com>
2019-03-11 01:35:26 -0700

    idk

---
+++ clickygame/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AnImage from "./images"
+
+class App extends Component {
+  render() {
+    return (
+      <AnImage />
+    );
+  }
+}
+
+export default App;
